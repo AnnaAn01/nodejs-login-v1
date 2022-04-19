f4450c0f5a8b37a5e9856470cb84cdb26c1a515d
Anna An <arzumanyan.an@gmail.com>
2022-04-19 11:01:27 +0400

    creating profile page for users

--- controllers/auth.js
+++ controllers/auth.js
@@ -35,6 +35,18 @@ exports.login = async (req, res) => {
           const token = jwt.sign({ id }, process.env.JWT_SECRET, {
             expiresIn: process.env.JWT_EXPIRES_IN,
           });
+
+          console.log("The token is: " + token);
+
+          const cookieOptions = {
+            expires: new Date(
+              Date.now() + process.env.JWT_COOKIE_EXPIRES * 24 * 60 * 60 * 1000
+            ),
+            httpOnly: true,
+          };
+
+          res.cookie("jwt", token, cookieOptions);
+          res.status(200).redirect("/");
         }
       }
     );
